5ce2013eca844db4693a1da07b9554ccdfb74fe4
luiztools <luizfduartejr@gmail.com>
2021-08-19 16:12:20 -0300

    04-middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use('/beholder', (req, res, next) => {
+    res.send('Hello Beholder');
+})
 
 app.use('/', (req, res, next) => {
     res.send('Hello World');
