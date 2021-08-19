b52f4d36f1d1335ce640c896a01801d50e2892e7
luiztools <luizfduartejr@gmail.com>
2021-08-19 15:19:05 -0300

    05-useState

--- frontend/src/public/Login/Login.js
+++ frontend/src/public/Login/Login.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function onChangeEmail(event) { setEmail(event.target.value); }
+
+    function onChangePassword(event) { setPassword(event.target.value); }
 
     return (
         <main>
@@ -26,7 +33,7 @@ function Login() {
                                         <span className="input-group-text" id="basic-addon1">
                                             <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                         </span>
-                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required />
+                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required onChange={onChangeEmail} />
                                     </div>
                                 </div>
                                 <div className="form-group">
@@ -36,7 +43,7 @@ function Login() {
                                             <span className="input-group-text" id="basic-addon2">
                                                 <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path></svg>
                                             </span>
-                                            <input type="password" placeholder="Password" className="form-control" id="password" required />
+                                            <input type="password" placeholder="Password" className="form-control" id="password" required onChange={onChangePassword} />
                                         </div>
                                     </div>
                                     <div className="d-flex justify-content-between align-items-top mb-4">
