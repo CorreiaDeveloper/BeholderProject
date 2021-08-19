287ecb35b2ed9981a741c451c66eca68a11add4d
luiztools <luizfduartejr@gmail.com>
2021-08-19 15:36:00 -0300

    07-refactor front

--- frontend/src/public/Login/Login.js
+++ frontend/src/public/Login/Login.js
@@ -7,17 +7,26 @@ function Login() {
     const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    function onChangeEmail(event) { setEmail(event.target.value); }
-
-    function onChangePassword(event) { setPassword(event.target.value); }
+    function onChangeInput(event) {
+        if (event.target.id === 'email')
+            setEmail(event.target.value);
+        else
+            setPassword(event.target.value);
+    }
 
     function onSubmit(event) {
         event.preventDefault();
-        
-        const isValid = doLogin(email, password);
-        if (isValid)
-            history.push('/settings')
+
+        doLogin(email, password)
+            .then(isValid => {
+                if (isValid)
+                    history.push('/settings')
+            })
+            .catch(err => {
+                setError(err);
+            })
     }
 
     return (
@@ -43,7 +52,7 @@ function Login() {
                                         <span className="input-group-text" id="basic-addon1">
                                             <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                         </span>
-                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required onChange={onChangeEmail} />
+                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required onChange={onChangeInput} />
                                     </div>
                                 </div>
                                 <div className="form-group">
@@ -53,7 +62,7 @@ function Login() {
                                             <span className="input-group-text" id="basic-addon2">
                                                 <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path></svg>
                                             </span>
-                                            <input type="password" placeholder="Password" className="form-control" id="password" required onChange={onChangePassword} />
+                                            <input type="password" placeholder="Password" className="form-control" id="password" required onChange={onChangeInput} />
                                         </div>
                                     </div>
                                     <div className="d-flex justify-content-between align-items-top mb-4">
@@ -69,6 +78,11 @@ function Login() {
                                 <div className="d-grid">
                                     <button type="submit" className="btn btn-gray-800">Sign In</button>
                                 </div>
+                                {
+                                    error ?
+                                        <div className="alert alert-danger mt-2">{error}</div>
+                                        : <React.Fragment></React.Fragment>
+                                }
                             </form>
                         </div>
                     </div>
--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,9 +1,11 @@
 export async function doLogin(email, password) {
-    if(email === 'contato@luiztools.com.br' 
-    && password === '123456'){
-        return true;
-    }
-    return false;
+    return new Promise((resolve, reject) => {
+        if(email === 'contato@luiztools.com.br' 
+        && password === '123456'){
+            return resolve(true);
+        }
+        return reject(`Invalid user and/or password!`);
+    })
 }
 
 export async function doLogout(token) {
