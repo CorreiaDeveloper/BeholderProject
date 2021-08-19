5beb6e3561fcebc94379bf7bf35e81c29c41a7f1
luiztools <luizfduartejr@gmail.com>
2021-08-19 16:19:26 -0300

    06-post login

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,19 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use('/beholder', (req, res, next) => {
-    res.send('Hello Beholder');
+app.post('/login', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email === 'contato@luiztools.com.br'
+        && password === '123456')
+        return res.sendStatus(200);
+    else
+        return res.sendStatus(401);
+})
+
+app.post('/logout', (req, res, next) => {
+    return res.sendStatus(200);
 })
 
 app.use('/', (req, res, next) => {
