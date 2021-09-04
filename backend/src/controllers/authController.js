8e9c3c7f2cc5fe7adf033ed51eb9fdc68bdef307
luiztools <luizfduartejr@gmail.com>
2021-09-03 22:03:16 -0300

    04 - bcrypt

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 function doLogin(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
     if (email === 'contato@luiztools.com.br'
-        && password === '123456') {
+        && bcrypt.compareSync(password, '$2a$12$7eYPuLs30rH/nBuH5ZiXSulDHzr8hOTnO1kllrtkLw0J1GnjYIc4S')) {
         const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
             expiresIn: parseInt(process.env.JWT_EXPIRES)
         });
