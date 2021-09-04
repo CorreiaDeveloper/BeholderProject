9b9312fbbbd8a58191a5c2b6df1555819cd0368d
luiztools <luizfduartejr@gmail.com>
2021-09-04 00:29:27 -0300

    02 - setup inicial

---
+++ backend/src/db.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'beholder',
+    process.env.DB_USER || 'root',
+    process.env.DB_PWD,
+    {
+        dialect: process.env.DB_DIALECT || 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3006,
+        logging: process.env.DB_LOGS === 'true'
+    });
+
+module.exports = sequelize;
