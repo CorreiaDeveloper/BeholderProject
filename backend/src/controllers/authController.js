5c1487d40e8165eba2032ac1ad08fffa833120ad
luiztools <luizfduartejr@gmail.com>
2021-09-03 21:45:11 -0300

    02 - jwt back

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,10 +1,16 @@
+const jwt = require('jsonwebtoken');
+
 function doLogin(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
     if (email === 'contato@luiztools.com.br'
-        && password === '123456')
-        return res.sendStatus(200);
+        && password === '123456') {
+        const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+            expiresIn: parseInt(process.env.JWT_EXPIRES)
+        });
+        return res.json({ token });
+    }
     else
         return res.sendStatus(401);
 }
