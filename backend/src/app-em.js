cf6fec6838f9d0e1c7c99f53cfb09f62340d4f1f
luiztools <luizfduartejr@gmail.com>
2021-09-06 16:03:29 -0300

    09 - plugando WS

--- backend/src/app-em.js
+++ backend/src/app-em.js
@@ -1,12 +1,20 @@
 const crypto = require('./utils/crypto');
 
-module.exports = (settings) => {
+module.exports = (settings, wss) => {
     if (!settings) throw new Error(`You can't init the Exchange Monitor App without his settings. Check your database and/or startup code.`);
 
     settings.secretKey = crypto.decrypt(settings.secretKey);
     const exchange = require('./utils/exchange')(settings);
 
     exchange.miniTickerStream(markets => {
-        console.log(markets);
+        //console.log(markets);
+        if (!wss || !wss.clients) return;
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ miniTicker: markets }));
+            }
+        });
     })
+
+    console.log('App Exchange Monitor is running!');
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,17 @@ const database = require('./db');
 const app = require('./app');
 const settingsRepository = require('./repositories/settingsRepository');
 const appEm = require('./app-em');
+const appWs = require('./app-ws');
 
 settingsRepository.getDefaultSettings()
     .then(settings => {
-        appEm(settings);
-
-        app.listen(process.env.PORT, () => {
+        const server = app.listen(process.env.PORT, () => {
             console.log('App is running at ' + process.env.PORT);
         })
+
+        const wss= appWs(server);
+
+        appEm(settings, wss);
     })
     .catch(err => {
         console.error(err);
