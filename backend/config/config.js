602bf72db72b49abfed0c03dbc895ac1ef23f78d
luiztools <luizfduartejr@gmail.com>
2021-11-28 17:26:46 -0300

    ssl no mysql

--- backend/config/config.js
+++ backend/config/config.js
@@ -24,6 +24,12 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 };
