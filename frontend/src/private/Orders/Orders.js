5ce100750c09891e0ad5704e010afda68f912329
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2024-07-03 19:51:29 -0300

    Finalizando o Frontend de Orders

---
+++ frontend/src/private/Orders/Orders.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import Menu from '../../components/Menu/Menu';
+import NewOrderButton from '../../components/NewOrder/NewOrderButton';
+import NewOrderModal from '../../components/NewOrder/NewOrderModal';
+import { getBalance } from '../../services/ExchangeService';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { getOrders } from '../../services/OrdersService';
+import OrderRow from './OrderRow';
+import OrdersPagination from './OrdersPagination';
+import SearchSymbol from '../../components/SearchSymbol/SearchSymbol';
+
+function Orders() {
+
+    const { symbol } = useParams();
+
+    const [search, setSearch] = useState(symbol || '');
+
+    const defaultLocation = useLocation();
+
+    function getPage(location) {
+        if (!location) location = defaultLocation;
+        return new URLSearchParams(location.search).get('page');
+    }
+
+    const history = useHistory();
+    const [balances, setBalances] = useState({});
+    const [orders, setOrders] = useState([]);
+    const [count, setCount] = useState(0);
+    const [page, setPage] = useState(parseInt(getPage()));
+
+    function errorProcedure(err) {
+        if (err.response && err.response.status === 401) return history.push('/')
+        console.error(err);
+    }
+
+    function getBalanceCall(token) {
+        getBalance(token)
+            .then(info => {
+                const balances = Object.entries(info).map(item => {
+                    return {
+                        symbol: item[0],
+                        available: item[1].available,
+                        onOrder: item[1].onOrder
+                    }
+                })
+
+                setBalances(balances);
+            })
+            .catch(err => errorProcedure(err))
+    }
+
+    function getOrdersCall(token) {
+        getOrders(search, page || 1, token)
+            .then(result => {
+                setOrders(result.rows);
+                setCount(result.count)
+            })
+            .catch(err => errorProcedure())
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        getBalanceCall(token);
+        getOrdersCall(token);
+    }, [page, search])
+
+    useEffect(() => {
+        return history.listen(location => {
+            setPage(getPage(location));
+        })
+    }, [history])
+
+    function onOrderSubmit() {
+        history.go(0);
+    }
+
+    function onSearchChange(event) {
+        setSearch(event.target.value);
+    }
+
+    return (
+        <React.Fragment>
+            <Menu />
+            <main className='content'>
+                <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4'>
+                    <div className='d-block mb-4 mb-md-0'>
+                        <h2 className='h4'>Orders</h2>
+                    </div>
+                    <div className='btn-toolbar mb-2 mb-md-0'>
+                        <div className='d-inline-flex align-items-center'>
+                            <NewOrderButton />
+                            <div className="btn-group ms-2 ms-lg-3">
+                                <SearchSymbol onChange={onSearchChange} placeholder={search} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='card card-body border-0 shadow table-wrapper table-responsive'>
+                    <table className='table table-hover'>
+                        <thead>
+                            <tr>
+                                <th className='border-gray-200'>Symbol</th>
+                                <th className='border-gray-200'>Date</th>
+                                <th className='border-gray-200'>Side</th>
+                                <th className='border-gray-200'>Qty</th>
+                                <th className='border-gray-200'>Net</th>
+                                <th className='border-gray-200'>Status</th>
+                                <th className='border-gray-200'>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                orders
+                                    ? orders.map(order => (
+                                        <OrderRow key={order.clientOrderId} data={order} />
+                                    ))
+                                    : <React.Fragment></React.Fragment>
+                            }
+                        </tbody>
+                    </table>
+                    <OrdersPagination count={count} />
+                </div>
+            </main>
+            <NewOrderModal wallet={balances} onSubmit={onOrderSubmit} />
+        </React.Fragment>
+    );
+}
+
+export default Orders;
---
+++ frontend/src/private/Orders/OrdersPagination.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const PAGE_SIZE = 10;
+
+/**
+ * props:
+ * - count
+*/
+
+function OrdersPagination(props){
+
+    function useQuery(){
+        return new URLSearchParams(useLocation().search);
+    }
+
+    const query = useQuery();
+
+    function getPageClass(page){
+        const queryPage = query.get('page');
+        const isActive = queryPage == page || (!queryPage && page === 1);
+        return isActive ? "page-item active" : "page-item";
+    }
+
+    let pagesQty = Math.ceil(props.count / PAGE_SIZE);
+    pagesQty = pagesQty > 10 ? 10 : pagesQty;
+    const pages = [];
+    for (let i = 1; i <= pagesQty; i++)
+        pages.push(i);
+
+    function getPageLink(page){
+        return `${window.location.pathname}?page=${page}`;
+    }
+
+    return (
+        <div className="card-footer px-3 border-0 d-flex flex-column flex-lg-row align-items-center justify-content-between">
+            <nav aria-label="Page navigation">
+                <ul className="pagination mb-0">
+                    {
+                        pages.map(p => (
+                            <li key={p} className={getPageClass(p)}>
+                                <Link className="page-link" to={getPageLink(p)}>{p}</Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </nav>
+            <div className="fw-normal small mt-4 mt-lg-0">
+                <b>
+                    {props.count} result(s).
+                </b>
+            </div>
+        </div>
+    )
+}
+
+export default OrdersPagination;
